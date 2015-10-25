4fee1a06628b5b444291b55e12092589e54f2bb1
lamazavr <lamazavr@localhost.localdomain>
2015-10-25 22:47:52 +0300

    meta nodejs и libmodbus

--- olinuxino-a10-lime/init.sh
+++ olinuxino-a10-lime/init.sh
@@ -5,6 +5,8 @@
 git clone -b fido git://git.yoctoproject.org/poky.git
 cd poky
 git clone -b fido https://github.com/linux-sunxi/meta-sunxi.git
+git clone https://git.congatec.com/yocto/meta-openembedded.git
+git clone https://github.com/imyller/meta-nodejs.git
 
 #MACHINE=olinuxino-a10lime
 hob
