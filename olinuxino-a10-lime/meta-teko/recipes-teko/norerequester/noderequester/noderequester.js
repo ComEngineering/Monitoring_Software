1cab3e7062a541f96345507edc5d7277c66657f3
lamazavr <lamazavr@localhost.localdomain>
2015-12-08 23:30:55 +0200

    2b команда, доработал скрипт опроса для поочередного выполнения

--- olinuxino-a10-lime/meta-teko/recipes-teko/norerequester/noderequester/noderequester.js
+++ olinuxino-a10-lime/meta-teko/recipes-teko/norerequester/noderequester/noderequester.js
@@ -1,47 +1,67 @@
 var fs = require('fs');
 var mysql = require('mysql');
-var modbus = require('modbus');
+var modbus = require('/opt/teko/olinuxino-a10-lime/meta-teko/recipes-teko/nodemobdbus/nodemodbus/build/Release/modbus');
 
-
+var obj;
+var mb;
 
 var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : ''
+    host     : 'localhost',
+    user     : 'root',
+    password : 'newpass',
 });
 
 console.log("Initialising mysql connection ...");
 connection.connect(function(err) {
-  if (err) {
-    console.error('error connecting: ' + err.stack);
-    return;
-  }
+    if (err) {
+        console.error('error connecting: ' + err.stack);
+        return;
+    }
 
-  console.log('connected as id ' + connection.threadId);
-});
+    console.log('Connected as id ' + connection.threadId);
 
-console.log("Initialising mysql db ...");
-// пробуем создать базу данных, на случай если её нет
-connection.query('CREATE DATABASE IF NOT EXISTS `teko` CHARACTER SET utf8 COLLATE utf8_general_ci');
-connection.query('USE teko');
-connection.query('CREATE TABLE IF NOT EXISTS `mb_parameters` (`id` INT(11) NOT NULL AUTO_INCREMENT, `name` CHAR(30) NOT NULL, `jsobject` TEXT NOT NULL, `timestamp` TIMESTAMP NOT NULL, PRIMARY KEY(`id`))');
+    //если соеденились настраиваем модбас и начинаем опрашивать
+    main();
+    });
 
 
-
-
-
-console.log("Initialising mb_requsting ...");
-
-try {
-    var mb = new mb.MbObject();
-    mb.Connect("/dev/ttyUSB0", 9600, 1);
-
-    //пробуем загрузить файл с описанием устройств
-    var obj = JSON.parse(fs.readFileSync('mb_conf.json', 'utf8'));
-} catch(e) 
+function main()
 {
-    console.log(e);
-    process.exit(1);
+
+    console.log("Initialising mysql db ...");
+    // пробуем создать базу данных, на случай если её нет
+    connection.query('CREATE DATABASE IF NOT EXISTS `teko` CHARACTER SET utf8 COLLATE utf8_general_ci');
+    connection.query('USE teko');
+    connection.query('CREATE TABLE IF NOT EXISTS `mb_parameters` (`id` INT(11) NOT NULL AUTO_INCREMENT, `name` CHAR(30) NOT NULL, `jsobject` TEXT NOT NULL, `timestamp` TIMESTAMP NOT NULL, PRIMARY KEY(`id`))', function(err){
+        if (err)
+        {
+            console.log('error mysql initialisation: ' + err.stack);
+            process.exit(1);
+        }
+        init_mb_req();
+    });
+}
+
+//создаем модбас соединение
+function init_mb_req()
+{
+    console.log("Initialising mb_requsting ...");
+
+    try {
+        mb = new modbus.MbObject();
+        mb.Connect("/dev/ttyTEST0", 9600, 1);
+
+        //пробуем загрузить файл с описанием устройств
+        obj = JSON.parse(fs.readFileSync('example.mb_conf.js', 'utf8'));
+    } catch(e) 
+    {
+        console.log(e);
+        process.exit(1);
+    }
+
+    console.log("Starting parameters update ...");
+    //начинаем читать параметры
+    update_parameters();
 }
 
 //функция чтения/записи параметров
@@ -74,6 +94,3 @@ function sleep(ms, callback){
    setTimeout(function(){callback();}, ms);
 }
 
-console.log("Starting parameters update ...");
-//начинаем читать параметры
-update_parameters();
