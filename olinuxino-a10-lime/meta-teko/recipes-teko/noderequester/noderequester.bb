00358d9073784faf87977419d1d282d3ed27b0f9
lamazavr <lamazavr@localhost.localdomain>
2016-01-10 23:30:51 +0200

    отладка на плате

---
+++ olinuxino-a10-lime/meta-teko/recipes-teko/noderequester/noderequester.bb
@@ -0,0 +1,29 @@
+#
+# This file was derived from the 'Hello World!' example recipe in the
+# Yocto Project Development Manual.
+#
+
+DESCRIPTION = "Modbus parameters requester for teko monitoring project"
+SECTION = "teko"
+LICENSE = "MIT"
+LIC_FILES_CHKSUM = "file://${COMMON_LICENSE_DIR}/MIT;md5=0835ade698e0bcf8506ecda2f7b4f302"
+PR = "r0"
+
+inherit npm
+
+DEPENDS = "nodejs nodemodbus node-mysql"
+
+SRC_URI = "file://noderequester.js \
+file://example.mb_conf.js"
+
+S = "${WORKDIR}"
+
+do_install() {
+    install -D -m 0755 ${S}/noderequester.js ${D}/opt/noderequester.js
+}
+
+FILES_${PN} += "/opt/noderequester.js"
+
+# Prevents do_package failures with:
+# debugsources.list: No such file or directory:
+INHIBIT_PACKAGE_DEBUG_SPLIT = "1"
---
+++ olinuxino-a10-lime/meta-teko/recipes-teko/noderequester/noderequester/noderequester.js
@@ -0,0 +1,172 @@
+var fs = require('fs');
+var mysql = require('mysql');
+var modbus = require('modbus');
+var dbus = require('dbus-native');
+
+var obj;
+var mb;
+
+var bus = dbus.sessionBus();
+var name = 'teko.modbus';
+bus.requestName(name, 0);
+
+var tekoIface = {
+    name: 'com.teko.modbus',
+    //for debug 
+    methods: {
+        doStuff: ['s', 's'],
+        timesTwo: ['d', 'd'],
+        respondWithDouble: ['s', 'd']
+    },
+    signals: {
+        testsignal: [ 'us', 'name1', 'name2' ]
+    },
+    properties: {
+       TestProperty: 'y'
+    }
+};
+//TODO: create write register function here
+var teko_dbus = {
+    respondWithDouble: function(s) {
+        console.log('Received "' + s + "'");
+        return 3.14159;
+    },
+    timesTwo: function(d) {
+    console.log(d);
+        return d*2; 
+    },
+    doStuff: function(s) {
+        return 'Received "' + s + '" - this is a reply'; 
+    },
+    TestProperty: 42,
+    emit: function(name, param1, param2) {
+        console.log('signal emit', name, param1, param2);
+    }
+};
+bus.exportInterface(teko_dbus, '/com/teko/modbus', tekoIface);
+
+
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'newpass',
+    socketPath: '/var/lib/mysql/mysql.sock',
+});
+
+console.log("Initialising mysql connection ...");
+connection.connect(function(err) {
+    if (err) {
+        console.error('error connecting: ' + err.stack);
+        return;
+    }
+
+    console.log('Connected as id ' + connection.threadId);
+
+    //если соеденились настраиваем модбас и начинаем опрашивать
+    main();
+    });
+
+function Int16toNumber(x)
+{
+    x = x << 16;
+    x = x >> 0;
+    return x >> 16;
+}
+
+function main()
+{
+
+    console.log("Initialising mysql db ...");
+    // пробуем создать базу данных, на случай если её нет
+    connection.query('CREATE DATABASE IF NOT EXISTS `teko` CHARACTER SET utf8 COLLATE utf8_general_ci');
+    connection.query('USE teko');
+    connection.query('CREATE TABLE IF NOT EXISTS `mb_parameters` (`id` INT(11) NOT NULL AUTO_INCREMENT, `name` CHAR(30) NOT NULL, `jsobject` TEXT NOT NULL, `timestamp` TIMESTAMP NOT NULL, PRIMARY KEY(`id`))', function(err){
+        if (err)
+        {
+            console.log('error mysql initialisation: ' + err.stack);
+            process.exit(1);
+        }
+        init_mb_req();
+    });
+}
+
+//создаем модбас соединение
+function init_mb_req()
+{
+    console.log("Initialising mb_requsting ...");
+
+    try {
+        mb = new modbus.MbObject();
+        mb.Connect("/dev/ttyS1", 9600, 1);
+
+        //пробуем загрузить файл с описанием устройств
+        obj = JSON.parse(fs.readFileSync('/opt/example.mb_conf.js', 'utf8'));
+    } catch(e) 
+    {
+        console.log(e);
+        process.exit(1);
+    }
+
+    obj.input.values.forEach(function(e){
+        e.value = 0;
+    });
+
+
+    console.log("Starting parameters update ...");
+    //начинаем читать параметры
+    update_parameters();
+}
+
+//функция чтения/записи параметров
+function update_parameters()
+{
+    obj.input.values.forEach(function(e){
+        //тут обмен с устройствами и запись в базу данных
+        console.log(e.description);
+        try{
+            var regs = mb.ReadRegisters(e.addr, e.reg, 1);
+            if (e.convert != null)
+	    {                                          
+	        var func = new Function('return ' + e.convert)();
+	    	regs[0] = func(regs[0]);
+	    }
+	    console.log("Got "+e.name + ": " + regs)
+       
+	    if (regs[0] != e.value) 
+            {	
+//		if (e.convert != null)
+			regs[0] = e.convert(regs[0])
+
+
+                e.value = regs[0];
+                var dbstring = JSON.stringify(e);
+                //тут сделать запись объекта
+                connection.query("INSERT INTO `mb_parameters` (`name`, `jsobject`) values (?, ?)", [e.name, dbstring], function (error, results, fields) {
+                  // error will be an Error if one occurred during the query
+                  // results will contain the results of the query
+                  // fields will contain information about the returned results fields (if any)
+                  if (error)
+                  {
+                    console.log("ERROR WRITING:" + results);
+                  }
+
+                  //send modbus update signal via dbus
+                  //teko_dbus.emit('testsignal', Date.now(), 'param2');
+
+                });
+            }
+        } catch (err) 
+        {
+            //console.log(err);
+        }
+    });
+    
+    //пока обновляем все вместе
+    sleep(100, update_parameters);
+}
+
+//обертка для вызова функции по таймеру
+function sleep(ms, callback){
+   setTimeout(function(){callback();}, ms);
+}
+
