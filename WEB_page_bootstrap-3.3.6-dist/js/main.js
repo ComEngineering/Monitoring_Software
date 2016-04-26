66563618e635a5e34a1ee6293e760ea890977527
trubochkin <lexich1990@gmail.com>
2016-04-26 17:28:23 +0300

    Р”РѕР±Р°РІРёР» С‚РµСЃС‚РѕРІСѓСЋ Web СЃС‚СЂР°РЅРёС†Сѓ

---
+++ WEB_page_bootstrap-3.3.6-dist/js/main.js
@@ -0,0 +1,16 @@
+$(function () {
+	//BEGIN BACK TO TOP
+    $(window).scroll(function(){
+        if ($(this).scrollTop() < 200) {
+            $('#totop') .fadeOut();
+        } else {
+            $('#totop') .fadeIn();
+        }
+    });
+    $('#totop').on('click', function(){
+        $('html, body').animate({scrollTop:0}, 'fast');
+        return false;
+    });
+    //END BACK TO TOP
+
+});
