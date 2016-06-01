03452fba61c9b8a3fc4e2913859bdaab29d7fea0
Dmitrey Salnikov <mr.dimas@meta.ua>
2016-06-01 16:55:40 +0300

    configuration file update

--- olinuxino-a10-lime/meta-teko/recipes-teko/noderequester/noderequester/example.mb_conf.js
+++ olinuxino-a10-lime/meta-teko/recipes-teko/noderequester/noderequester/example.mb_conf.js
@@ -273,6 +273,240 @@
                 "min": 0,
                 "max": 65535
             }
+        }, {
+            "description": "Задание внутренней температуры",
+            "name": "conditioner_int_temp_ref",
+            "addr": 1,
+            "reg": 20,
+            "oid": ".1.3.6.1.4.1.47480.4.1",
+            "default": 30,
+            "type": "modbus",
+            "bus": "modbus_external",
+            "intervals": {
+                "min": 10,
+                "max": 40
+            }
+        }, {
+            "description": "DT вкл/выкл компрессора",
+            "name": "conditioner_dt_comp",
+            "addr": 1,
+            "reg": 21,
+            "oid": ".1.3.6.1.4.1.47480.4.2",
+            "default": 5,
+            "type": "modbus",
+            "bus": "modbus_external",
+            "intervals": {
+                "min": 5,
+                "max": 20
+            }
+        }, {
+            "description": "Задание температуры ta1",
+            "name": "conditioner_ta1_ref",
+            "addr": 1,
+            "reg": 22,
+            "oid": ".1.3.6.1.4.1.47480.4.3",
+            "default": 31,
+            "type": "modbus",
+            "bus": "modbus_external",
+            "intervals": {
+                "min": 20,
+                "max": 90
+            }
+        }, {
+            "description": "Задание аварийно-высокой температуры в шкафу",
+            "name": "conditioner_fail_t_ref",
+            "addr": 1,
+            "reg": 23,
+            "oid": ".1.3.6.1.4.1.47480.4.4",
+            "default": 40,
+            "type": "modbus",
+            "bus": "modbus_external",
+            "intervals": {
+                "min": 20,
+                "max": 90
+            }
+        }, {
+            "description": "Задание температуры th1",
+            "name": "conditioner_th1_ref",
+            "addr": 1,
+            "reg": 24,
+            "oid": ".1.3.6.1.4.1.47480.4.5",
+            "default": 18,
+            "type": "modbus",
+            "bus": "modbus_external",
+            "intervals": {
+                "min": 5,
+                "max": 30
+            }
+        }, {
+            "description": "Задание аварийно-низкой температуры в шкафу th2",
+            "name": "conditioner_th2_ref",
+            "addr": 1,
+            "reg": 25,
+            "oid": ".1.3.6.1.4.1.47480.4.6",
+            "default": 7,
+            "type": "modbus",
+            "bus": "modbus_external",
+            "intervals": {
+                "min": 5,
+                "max": 30
+            }
+        }, {
+            "description": "Задание аварийной температуры хладагента t7",
+            "name": "conditioner_t7_ref",
+            "addr": 1,
+            "reg": 28,
+            "oid": ".1.3.6.1.4.1.47480.4.7",
+            "default": 60,
+            "type": "modbus",
+            "bus": "modbus_external",
+            "intervals": {
+                "min": 30,
+                "max": 90
+            }
+        }, {
+            "description": "DT аварийной температуры хладагента",
+            "name": "conditioner_dt_hlad_ref",
+            "addr": 1,
+            "reg": 29,
+            "oid": ".1.3.6.1.4.1.47480.4.8",
+            "default": 5,
+            "type": "modbus",
+            "bus": "modbus_external",
+            "intervals": {
+                "min": 5,
+                "max": 30
+            }
+        }, {
+            "description": "Задание температуры для зоны + конденсатора",
+            "name": "conditioner_p_ref",
+            "addr": 1,
+            "reg": 30,
+            "oid": ".1.3.6.1.4.1.47480.4.9",
+            "default": 30,
+            "type": "modbus",
+            "bus": "modbus_external",
+            "intervals": {
+                "min": 30,
+                "max": 50
+            }
+        }, {
+            "description": "Задание температуры для зоны ++ конденсатора",
+            "name": "conditioner_pp_ref",
+            "addr": 1,
+            "reg": 31,
+            "oid": ".1.3.6.1.4.1.47480.4.10",
+            "default": 32,
+            "type": "modbus",
+            "bus": "modbus_external",
+            "intervals": {
+                "min": 30,
+                "max": 50
+            }
+        }, {
+            "description": "Задание температуры для зоны - конденсатора",
+            "name": "conditioner_m_ref",
+            "addr": 1,
+            "reg": 32,
+            "oid": ".1.3.6.1.4.1.47480.4.11",
+            "default": 25,
+            "type": "modbus",
+            "bus": "modbus_external",
+            "intervals": {
+                "min": 20,
+                "max": 50
+            }
+        }, {
+            "description": "Задание температуры для зоны -- конденсатора",
+            "name": "conditioner_mm_ref",
+            "addr": 1,
+            "reg": 33,
+            "oid": ".1.3.6.1.4.1.47480.4.12",
+            "default": 23,
+            "type": "modbus",
+            "bus": "modbus_external",
+            "intervals": {
+                "min": 15,
+                "max": 50
+            }
+        }, {
+            "description": "Задание интервала времени для зоны + конденсатора",
+            "name": "conditioner_p_cond_ref",
+            "addr": 1,
+            "reg": 34,
+            "oid": ".1.3.6.1.4.1.47480.4.13",
+            "default": 15,
+            "type": "modbus",
+            "bus": "modbus_external",
+            "intervals": {
+                "min": 1,
+                "max": 300
+            }
+        }, {
+            "description": "Задание интервала времени для зоны ++ конденсатора",
+            "name": "conditioner_pp_cond_ref",
+            "addr": 1,
+            "reg": 35,
+            "oid": ".1.3.6.1.4.1.47480.4.14",
+            "default": 5,
+            "type": "modbus",
+            "bus": "modbus_external",
+            "intervals": {
+                "min": 1,
+                "max": 300
+            }
+        }, {
+            "description": "Задание интервала времени для зоны - конденсатора",
+            "name": "conditioner_m_cond_ref",
+            "addr": 1,
+            "reg": 36,
+            "oid": ".1.3.6.1.4.1.47480.4.15",
+            "default": 130,
+            "type": "modbus",
+            "bus": "modbus_external",
+            "intervals": {
+                "min": 1,
+                "max": 300
+            }
+        }, {
+            "description": "Задание температуры для включения фрикулига",
+            "name": "conditioner_freecool_ref",
+            "addr": 1,
+            "reg": 37,
+            "oid": ".1.3.6.1.4.1.47480.4.16",
+            "default": 15,
+            "type": "modbus",
+            "bus": "modbus_external",
+            "intervals": {
+                "min": 0,
+                "max": 50
+            }
+        }, {
+            "description": "Задание скорости вентилятора конденсатора",
+            "name": "conditioner_fan_speed_ref",
+            "addr": 1,
+            "reg": 44,
+            "oid": ".1.3.6.1.4.1.47480.4.17",
+            "default": 0,
+            "type": "modbus",
+            "bus": "modbus_external",
+            "intervals": {
+                "min": 0,
+                "max": 100
+            }
+        }, {
+            "description": "Задание скорости вентилятора испарителя",
+            "name": "conditioner_fan2_speed_ref",
+            "addr": 1,
+            "reg": 45,
+            "oid": ".1.3.6.1.4.1.47480.4.18",
+            "default": 0,
+            "type": "modbus",
+            "bus": "modbus_external",
+            "intervals": {
+                "min": 0,
+                "max": 100
+            }
         }]
     }
 }
